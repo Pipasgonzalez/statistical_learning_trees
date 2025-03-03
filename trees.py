876de95a19cece564e943977453b50d44baf75d4
pipasgonzalez <nicolasjaegergallego@googlemail.com>
2025-03-03 15:32:10 +0100

    Third Push

--- trees.py
+++ trees.py
@@ -4,3 +4,9 @@ import pandas as pd
 data_matrix = pd.read_csv("flowering_time.csv")
 target_matrix = pd.read_csv("data.csv")
 print("Import Successful")
+
+
+# 0. Sample 600 rows randomly
+sampled_rows = data_matrix.sample(n = 600)
+
+# 1. Normal Naive Decision Tree
