00f15d3f6f24e780b0f4ce98222e521fe3dde249
pipasgonzalez <nicolasjaegergallego@googlemail.com>
2025-03-03 15:48:42 +0100

    Fourth Push

--- trees.py
+++ trees.py
@@ -2,11 +2,15 @@ import sklearn
 import pandas as pd
 
 data_matrix = pd.read_csv("flowering_time.csv")
-target_matrix = pd.read_csv("data.csv")
-print("Import Successful")
 
+# Transform Target Matrix
+target_matrix = pd.read_csv("data.csv")
+target_matrix = (target_matrix>40).astype(int)
+
+print("Import Successful")
 
 # 0. Sample 600 rows randomly
 sampled_rows = data_matrix.sample(n = 600)
+
 
 # 1. Normal Naive Decision Tree
